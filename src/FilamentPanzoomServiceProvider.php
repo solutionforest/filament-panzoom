d7998e47fe2e0f8be57d0c46317d01b00a237c99
Jackylwl <146152931+Jackylwl@users.noreply.github.com>
2025-09-03 05:44:33 +0000

    Fix styling

--- src/FilamentPanzoomServiceProvider.php
+++ src/FilamentPanzoomServiceProvider.php
@@ -4,7 +4,6 @@ namespace SolutionForest\FilamentPanzoom;
 
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
